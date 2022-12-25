e2443fe8029113dc6e8e62db49b912a56b651d57
yavuzyagis <m.yavuzyagis@gmail.com>
2022-12-25 21:36:23 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,3 +1,15 @@
+fn trial() {
+    let needle = 12;
+    let haystack = [1, 2, 3, 4, 5, 312, 4232, 12212, 4232, 1231];
+    for item in &haystack {
+        if *item == needle {
+            println!("{}", item);
+        } else {
+            println!("{} not found", item);
+        }
+    }
+}
+
 fn main() {
     let my_name = "yavuz";
     let my_surname = "yagis";
@@ -6,4 +18,5 @@ fn main() {
     let b = &my_surname;
 
     println!("My name is {} {}", *a, *b);
+    trial();
 }
