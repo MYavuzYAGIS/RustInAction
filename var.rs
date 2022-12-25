ead151167eabfba83240c977a519acb47b7f803d
yavuzyagis <m.yavuzyagis@gmail.com>
2022-12-25 17:41:08 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- var.rs
+++ var.rs
@@ -1,1 +1,14 @@
+use std::ops::Add;
+fn main() {
+    let a = 10;
+    let b: i32 = 20;
+    let c = 30i32;
+    let d = 30_i32;
+    let e = add(a, b).add(c.add(d));
 
+    println!("(a+b) + (c + d) = {}", e);
+
+    fn add(i: i32, j: i32) -> i32 {
+        i + j
+    }
+}
