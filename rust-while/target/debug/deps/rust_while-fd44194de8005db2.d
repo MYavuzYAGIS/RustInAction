9f8c6483fff6fa53a06d906f5729c3147314b637
yavuzyagis <m.yavuzyagis@gmail.com>
2022-12-25 18:58:39 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

---
+++ rust-while/target/debug/deps/rust_while-fd44194de8005db2.d
@@ -0,0 +1,5 @@
+/Users/myyagis/Desktop/Rust in action/rust-while/target/debug/deps/rust_while-fd44194de8005db2: src/main.rs
+
+/Users/myyagis/Desktop/Rust in action/rust-while/target/debug/deps/rust_while-fd44194de8005db2.d: src/main.rs
+
+src/main.rs:
---
+++ rust-while/target/debug/rust-while.d
@@ -0,0 +1,1 @@
+/Users/myyagis/Desktop/Rust\ in\ action/rust-while/target/debug/rust-while: /Users/myyagis/Desktop/Rust\ in\ action/rust-while/src/main.rs
