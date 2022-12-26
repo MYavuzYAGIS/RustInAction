374d4790d0d64e4c34a5d2b0834a5f7f14c6d15d
yavuzyagis <m.yavuzyagis@gmail.com>
2022-12-26 10:15:12 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

---
+++ collections/target/debug/deps/collections-7a72c0afc160e73a.d
@@ -0,0 +1,5 @@
+/Users/myyagis/Desktop/Rust in action/collections/target/debug/deps/collections-7a72c0afc160e73a.rmeta: src/main.rs
+
+/Users/myyagis/Desktop/Rust in action/collections/target/debug/deps/collections-7a72c0afc160e73a.d: src/main.rs
+
+src/main.rs:
---
+++ collections/target/debug/deps/collections-990c90e481ee0d65.d
@@ -0,0 +1,5 @@
+/Users/myyagis/Desktop/Rust in action/collections/target/debug/deps/collections-990c90e481ee0d65.rmeta: src/main.rs
+
+/Users/myyagis/Desktop/Rust in action/collections/target/debug/deps/collections-990c90e481ee0d65.d: src/main.rs
+
+src/main.rs:
