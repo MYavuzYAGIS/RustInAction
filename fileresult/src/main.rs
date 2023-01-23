99f63bb1fafc44f23041807a3e84a89c08f9275d
yavuzyagis <m.yavuzyagis@gmail.com>
2023-01-23 18:09:41 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

---
+++ fileresult/src/main.rs
@@ -0,0 +1,44 @@
+use rand::prelude::*;
+
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator)
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.date = data.clone();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_1000) {
+        let err_msg = String::from("Permission Denied");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn close(f: File) -> Result<File, String> {}
---
+++ fileresult/target/debug/build/libc-f264d61f72382fb6/build_script_build-f264d61f72382fb6.d
@@ -0,0 +1,5 @@
+/Users/myyagis/Desktop/Rust in action/fileresult/target/debug/build/libc-f264d61f72382fb6/build_script_build-f264d61f72382fb6: /Users/myyagis/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
+
+/Users/myyagis/Desktop/Rust in action/fileresult/target/debug/build/libc-f264d61f72382fb6/build_script_build-f264d61f72382fb6.d: /Users/myyagis/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs
+
+/Users/myyagis/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.139/build.rs:
